ce93e2021099dde2505e68826c30fc62caeefdd0
eScape LLC <winker9@hotmail.com>
2025-12-16 12:22:41 -0500

    new service for time of day; decouple from system datetime.

---
+++ python/model/time_of_day.py
@@ -0,0 +1,23 @@
+
+import datetime
+from typing import Protocol, runtime_checkable
+
+@runtime_checkable
+class TimeOfDay(Protocol):
+	"""
+	This exists primarily so we can scale down elapsed time during tests.
+	For example, one second of "real" time may scale to one hour of "internal" time.
+	Likewise, any computed timedelta objects must be scaled the same way.
+	For example, setting a timer for one hour will trigger after one second of "real" time.
+	The system MUST NOT directly reference "datetime.datetime.now()" or variants.
+	"""
+	def current_time(self) -> datetime.datetime:
+		...
+	def current_time_utc(self) -> datetime.datetime:
+		...
+
+class SystemTimeOfDay(TimeOfDay):
+	def current_time(self) -> datetime.datetime:
+		return datetime.datetime.now()
+	def current_time_utc(self) -> datetime.datetime:
+		return datetime.datetime.now(datetime.timezone.utc)
