be3c85b84972aa7e3f7db183aee1baa6bf41891d
eScape LLC <winker9@hotmail.com>
2026-02-25 09:14:38 -0500

    mock_display clean folder on startup.

--- python/display/mock_display.py
+++ python/display/mock_display.py
@@ -1,11 +1,13 @@
-from datetime import datetime
 import os
+import logging
+import shutil
+from datetime import datetime
 from typing import cast
 from pathvalidate import sanitize_filename
-import logging
 from PIL import Image
 from .display_base import DisplayBase
 from ..model.configuration_manager import ConfigurationManager
+from pathlib import Path
 
 class MockDisplay(DisplayBase):
 	def __init__(self, name: str):
@@ -26,11 +28,21 @@ class MockDisplay(DisplayBase):
 	def shutdown(self):
 		pass
 
+	def clear_folder(self, folder: str):
+		self.logger.debug(f"clean folder: {folder}")
+		for item in Path(folder).iterdir():
+			if item.is_file() or item.is_symlink():
+				item.unlink()  # Delete file or link
+			elif item.is_dir():
+				shutil.rmtree(item)  # Delete subdirectory
+		pass
+
 	def render(self, img: Image.Image, id: int, title: str|None = None):
 		self.logger.info(f"'{self.name}' render id={id} title='{title}' img={img.width}x{img.height}")
 		if self.display_settings is None:
 			self.logger.error("No display_settings loaded")
 			return
+		clean_folder = cast(bool,self.display_settings.get("mock.cleanOutputFolder", False))
 		output_dir = cast(str,self.display_settings.get("mock.outputFolder", None))
 		if output_dir is None:
 			self.logger.error("output_dir is not defined")
@@ -40,13 +52,14 @@ class MockDisplay(DisplayBase):
 				self.logger.debug(f"output_dir Created: {output_dir}")
 			except Exception as e:
 				self.logger.error(f"output_dir {output_dir}: {e}")
-#		else:
+		else:
 #			self.logger.debug(f"output_dir exists: {output_dir}")
+			if id == 1 and clean_folder:
+				self.clear_folder(output_dir)
 
 		timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-#		filepath = os.path.join(output_dir, f"display_{timestamp}.png")
 		fn = sanitize_filename(title) if title else "untitled"
-		filepath = os.path.join(output_dir, f"display_{id:04d}_{timestamp}_{fn}.png")
+		filepath = os.path.join(output_dir, f"{id:04d}_{timestamp}_{fn}.png")
 		self.logger.info(f"save {filepath}")
 		img.save(filepath, "PNG")
 
