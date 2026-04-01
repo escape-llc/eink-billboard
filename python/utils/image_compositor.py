6c55537132912c0ffce73a59bbf873017686fc26
eScape LLC <winker9@hotmail.com>
2026-04-01 17:27:39 -0400

    compositor refactor.

--- python/utils/image_compositor.py
+++ python/utils/image_compositor.py
@@ -4,14 +4,13 @@ from PIL import Image, ImageDraw, ImageFont
 
 from ..task.display import DisplayImage
 
-type ImageLayerInfo = tuple[Image.Image, str]
-type RenderResult = tuple[bool, ImageLayerInfo|None]
 class ImageOverlay:
 	def __init__(self, image: Image.Image, position: tuple):
 		self.image = image
 		self.position = position
 	pass
 
+type LayerStackOp = tuple["LayerStack", "LayerStack"]
 @dataclass(frozen=True)
 class LayerStack:
 	version: int
@@ -19,68 +18,83 @@ class LayerStack:
 	overlays: list[ImageOverlay]
 	forground: DisplayImage|None
 	priority: DisplayImage|None
+	def set_foreground(self, di: DisplayImage|None) -> LayerStackOp:
+		return (self, LayerStack(self.version + 1, self.background, self.overlays, di, self.priority))
+	def set_background(self, di: DisplayImage|None) -> LayerStackOp:
+		return (self, LayerStack(self.version + 1, di, self.overlays, self.forground, self.priority))
+	def set_priority(self, di: DisplayImage|None) -> LayerStackOp:
+		return (self, LayerStack(self.version + 1, self.background, self.overlays, self.forground, di))
+	def set_overlays(self, overlays: list[ImageOverlay]) -> LayerStackOp:
+		return (self, LayerStack(self.version + 1, self.background, overlays, self.forground, self.priority))
 
+type RenderInfo = tuple[Image.Image, str]
 @runtime_checkable
 class RenderPackage(Protocol):
-	def render(self) -> RenderResult:
-		return (False, None)
+	def render(self) -> RenderInfo:
+		...
+	@property
+	def version(self) -> int:
+		...
 
 class SimpleRenderPackage(RenderPackage):
-	def __init__(self, di: DisplayImage):
+	def __init__(self, version:int, di: DisplayImage):
+		if di is None:
+			raise ValueError("SimpleRenderPackage requires a DisplayImage")
+		self._version = version
 		self._di = di
-	def render(self) -> RenderResult:
-		return (True, (self._di.img.copy(), self._di.title))
+	@property
+	def version(self) -> int:
+		return self._version
+	def render(self) -> RenderInfo:
+		return (self._di.img.copy(), self._di.title)
 
 class BgFgRenderPackage(RenderPackage):
-	def __init__(self, bg: DisplayImage, fg: DisplayImage):
+	def __init__(self, version:int, bg: DisplayImage, fg: DisplayImage):
+		if bg is None or fg is None:
+			raise ValueError("BgFgRenderPackage requires both background and foreground images")
+		if bg.img.size != fg.img.size:
+			raise ValueError("BgFgRenderPackage requires background and foreground images to be the same size")
+		self._version = version
 		self._bg = bg
 		self._fg = fg
-	def render(self) -> RenderResult:
-		if self._bg is None and self._fg is None:
-			return (False, None)
-		elif self._bg is not None and self._fg is not None:
-			img = Image.alpha_composite(self._bg.img.convert("RGBA"), self._fg.img.convert("RGBA"))
-			return (True, (img, self._fg.title))
-		elif self._bg is not None:
-			return (True, (self._bg.img.copy(), self._bg.title))
-		else:
-			return (True, (self._fg.img.copy(), self._fg.title))
+	@property
+	def version(self) -> int:
+		return self._version
+	def render(self) -> RenderInfo:
+		img = Image.alpha_composite(self._bg.img.convert("RGBA"), self._fg.img.convert("RGBA"))
+		return (img, self._fg.title)
 
 class ImageCompositor:
 	def __init__(self):
 		self._current_stack: LayerStack = LayerStack(0, None, [], None, None)
-		self._startVersion = 0
+		self._startVersion = self._current_stack.version
 	@property
 	def current_version(self) -> int:
 		return self._current_stack.version
-	def set_layer_background(self, image: DisplayImage|None) -> LayerStack:
-		previous = self._current_stack
-		self._current_stack = LayerStack(previous.version + 1, image, previous.overlays, previous.forground, previous.priority)
-		return previous
-	def set_layer_overlays(self, overlays: list[ImageOverlay]) -> LayerStack:
-		previous = self._current_stack
-		self._current_stack = LayerStack(previous.version + 1, previous.background, overlays, previous.forground, previous.priority)
-		return previous
-	def set_layer_forground(self, image: DisplayImage|None) -> LayerStack:
-		previous = self._current_stack
-		self._current_stack = LayerStack(previous.version + 1, previous.background, previous.overlays, image, previous.priority)
-		return previous
-	def set_layer_priority(self, image: DisplayImage|None) -> LayerStack:
-		previous = self._current_stack
-		self._current_stack = LayerStack(previous.version + 1, previous.background, previous.overlays, previous.forground, image)
-		return previous
+	def set_layer_background(self, bk: DisplayImage|None) -> LayerStackOp:
+		previous, self._current_stack = self._current_stack.set_background(bk)
+		return (previous, self._current_stack)
+	def set_layer_overlays(self, overlays: list[ImageOverlay]) -> LayerStackOp:
+		previous, self._current_stack = self._current_stack.set_overlays(overlays)
+		return (previous, self._current_stack)
+	def set_layer_forground(self, fg: DisplayImage|None) -> LayerStackOp:
+		previous, self._current_stack = self._current_stack.set_foreground(fg)
+		return (previous, self._current_stack)
+	def set_layer_priority(self, pri: DisplayImage|None) -> LayerStackOp:
+		previous, self._current_stack = self._current_stack.set_priority(pri)
+		return (previous, self._current_stack)
 	def is_dirty(self) -> bool:
 		return self._startVersion != self._current_stack.version
-	def render(self) -> RenderPackage|None:
+	def commit(self) -> RenderPackage|None:
 		if not self.is_dirty():
 			return None
 		self._startVersion = self._current_stack.version
 		if self._current_stack.priority is not None:
-			return SimpleRenderPackage(self._current_stack.priority)
+			return SimpleRenderPackage(self._current_stack.version, self._current_stack.priority)
 #		elif self._current_stack.forground is not None and self._current_stack.background is not None:
-#			return BgFgRenderPackage(self._current_stack.background, self._current_stack.forground)
+#			return BgFgRenderPackage(self._current_stack.version, self._current_stack.background, self._current_stack.forground)
 		elif self._current_stack.forground is not None:
-			return SimpleRenderPackage(self._current_stack.forground)
+			return SimpleRenderPackage(self._current_stack.version, self._current_stack.forground)
 		elif self._current_stack.background is not None:
-			return SimpleRenderPackage(self._current_stack.background)
+			return SimpleRenderPackage(self._current_stack.version, self._current_stack.background)
 		return None
